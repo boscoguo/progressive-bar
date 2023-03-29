dc8d567acf88f6bcf4b9902682adf2e8543cb165
bosco <boscoguo@hotmail.com>
2023-03-29 14:36:08 +1100

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { ProgressBarGroup } from "./components/progressBarGroup";
-import "antd/dist/antd.css";
 import "./index.scss";
 
 ReactDOM.render(
